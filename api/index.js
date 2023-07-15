189cce5b08ce6587c77534a1c138f85b7257b805
James Marshall <jameswhmarshallactions@gmail.com>
2023-07-15 12:54:24 +1000

    initial commit

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config('/api/.env');
+const port = process.env.PORT;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    body: 'This message is returned from the server - the app is working.',
+  });
+});
+
+if (port) {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app;
